09ef263f34af2a2a1b342eba5ddf8b40b8af1e10
Tom Astleitner <thomas@dextroid.at>
2021-10-22 13:54:00 +0200

    work in progress

--- nodes/shading.js
+++ nodes/shading.js
@@ -1,17 +1,20 @@
 module.exports = function(RED) {
 
     function ShadingOrientationNode(config) {
-        RED.nodes.createNode(this,config);
+		RED.nodes.createNode(this,config);
+		this.config = config;
     }
     RED.nodes.registerType("shading orientation",ShadingOrientationNode);
 
     function ShadingLocationNode(config) {
-        RED.nodes.createNode(this,config);
+		RED.nodes.createNode(this,config);
+		this.config = config;
     }
     RED.nodes.registerType("shading location",ShadingLocationNode);
 
     function ShadingConfigNode(config) {
         RED.nodes.createNode(this,config);
+		this.config = config;
     }
     RED.nodes.registerType("shading configuration",ShadingConfigNode);
 
@@ -22,33 +25,54 @@ module.exports = function(RED) {
     function ShadingNode(config) {
 
 		RED.nodes.createNode(this,config);
+
+		const loopIntervalTime = 5000;
+
 		var context = this.context();
 		var that = this;
 		var err = false;
 		var msgDebug = null;
-        var configSet = RED.nodes.getNode(config.configSet);
-        var configOrientation = RED.nodes.getNode(config.configOrientation);
+
+		config.set = RED.nodes.getNode(config.configSet).config;
+		config.orientation = RED.nodes.getNode(config.configOrientation).config;
+		config.location = RED.nodes.getNode(RED.nodes.getNode(config.configOrientation).config.config).config;
+		context.config = config;
+
+		context.config.set.inmsgButtonTopic = context.config.set.inmsgButtonTopic || "button";
+		context.config.set.inmsgWinswitchTopic = context.config.set.inmsgWinswitchTopic || "switch";
+
+		if (context.config.set.inmsgButtonPayloadOnType === 'num') {context.config.set.inmsgButtonPayloadOn = Number(config.togglePayload)}
+		else if (config.togglePayloadType === 'bool') {context.togglePayload = config.togglePayload === 'true'}
+		else {context.togglePayload = config.togglePayload};
+
+		if (config.node.inmsgButtonPayloadOnType === 'num') {config.node.inmsgButtonPayloadOn = Number(config.node.inmsgButtonPayloadOn)}
+		else if (config.node.inmsgButtonPayloadOnType === 'bool') {config.node.inmsgButtonPayloadOn = config.node.inmsgButtonPayloadOn === 'true'}
+		else {context.togglePayload = config.togglePayload};
+
+		function sendMsgDebugFunc(msg, reason) {
+			if (msg.debug) {
+				msgDebug = {
+					topic: "debug",
+					inmsg: msg,
+					config: config,
+					context: context,
+					reason: reason
+				}
+				that.send(msgDebug);
+			}
+		}
+		
+		let loopIntervalHandle = setInterval(function(){}, loopIntervalTime);
 
 		this.on('input', function(msg,send,done) {
 
-			function sendMsgDebugFunc(reason) {
-				if (msg.debug) {
-					msgDebug = {
-						topic: "debug",
-						inmsg: msg,
-						config: config,
-                        configSet: configSet,
-                        configOrientation: configOrientation,
-						context: context,
-						reason: reason
-					}
-					that.send(msgDebug);
-				}
-			}
+			if (msg.debug) {sendMsgDebugFunc(msg, "Debug solo")};
 
-			if (msg.debug) {
-				sendMsgDebugFunc("Debug solo");
-			}
+
+
+
+
+
 
 			if (err) {
 				if (done) {
