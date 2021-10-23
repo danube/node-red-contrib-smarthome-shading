beb13b17253a72e5bfaed386808c4800fc826258
Tom Astleitner <thomas@dextroid.at>
2021-10-23 22:08:29 +0200

    Added suncalc

--- nodes/shading.js
+++ nodes/shading.js
@@ -17,6 +17,7 @@ module.exports = function(RED) {
 		this.config = config;
     }
     RED.nodes.registerType("shading configuration",ShadingConfigNode);
+	
 
 
 
@@ -25,29 +26,18 @@ module.exports = function(RED) {
     function ShadingNode(config) {
 
 		RED.nodes.createNode(this,config);
-
+		
 		const loopIntervalTime = 5000;
-
 		var context = this.context();
 		var that = this;
 		var err = false;
 		var msgDebug = null;
-
+		var sunTimes;
+		var SunCalc = require("suncalc");
 		config.set = RED.nodes.getNode(config.configSet).config;
 		config.orientation = RED.nodes.getNode(config.configOrientation).config;
 		config.location = RED.nodes.getNode(RED.nodes.getNode(config.configOrientation).config.config).config;
-		context.config = config;
-
-		context.config.set.inmsgButtonTopic = context.config.set.inmsgButtonTopic || "button";
-		context.config.set.inmsgWinswitchTopic = context.config.set.inmsgWinswitchTopic || "switch";
-
-		if (context.config.set.inmsgButtonPayloadOnType === 'num') {context.config.set.inmsgButtonPayloadOn = Number(config.togglePayload)}
-		else if (config.togglePayloadType === 'bool') {context.togglePayload = config.togglePayload === 'true'}
-		else {context.togglePayload = config.togglePayload};
-
-		if (config.node.inmsgButtonPayloadOnType === 'num') {config.node.inmsgButtonPayloadOn = Number(config.node.inmsgButtonPayloadOn)}
-		else if (config.node.inmsgButtonPayloadOnType === 'bool') {config.node.inmsgButtonPayloadOn = config.node.inmsgButtonPayloadOn === 'true'}
-		else {context.togglePayload = config.togglePayload};
+		const configOriginal = config;
 
 		function sendMsgDebugFunc(msg, reason) {
 			if (msg.debug) {
@@ -55,14 +45,45 @@ module.exports = function(RED) {
 					topic: "debug",
 					inmsg: msg,
 					config: config,
+					configOriginal: configOriginal,
 					context: context,
 					reason: reason
 				}
 				that.send(msgDebug);
 			}
 		}
-		
-		let loopIntervalHandle = setInterval(function(){}, loopIntervalTime);
+	
+		function funSunCalc(){
+			sunTimes = SunCalc.getTimes(new Date(), 51.5, -0.1);
+			console.log(sunTimes);
+		}		
+
+		funSunCalc();
+		// let loopIntervalHandle = setInterval(funSunCalc, loopIntervalTime);
+
+		// Set replacement values for optional fields
+			config.set.inmsgButtonTopic = config.set.inmsgButtonTopic || "button";
+			config.set.inmsgWinswitchTopic = config.set.inmsgWinswitchTopic || "switch";
+
+		// Converting typed inputs
+			if (config.set.inmsgButtonPayloadOnType === 'num') {config.set.inmsgButtonPayloadOn = Number(config.set.inmsgButtonPayloadOn)}
+			else if (config.set.inmsgButtonPayloadOnType === 'bool') {config.set.inmsgButtonPayloadOn = config.set.inmsgButtonPayloadOn === 'true'}
+
+			if (config.set.inmsgButtonPayloadOffType === 'num') {config.set.inmsgButtonPayloadOff = Number(config.set.inmsgButtonPayloadOff)}
+			else if (config.set.inmsgButtonPayloadOffType === 'bool') {config.set.inmsgButtonPayloadOff = config.set.inmsgButtonPayloadOff === 'true'}
+
+			if (config.set.inmsgWinswitchPayloadOpenedType === 'num') {config.set.inmsgWinswitchPayloadOpened = Number(config.set.inmsgWinswitchPayloadOpened)}
+			else if (config.set.inmsgWinswitchPayloadOpenedType === 'bool') {config.set.inmsgWinswitchPayloadOpened = config.set.inmsgWinswitchPayloadOpened === 'true'}
+
+			if (config.set.inmsgWinswitchPayloadTiltedType === 'num') {config.set.inmsgWinswitchPayloadTilted = Number(config.set.inmsgWinswitchPayloadTilted)}
+			else if (config.set.inmsgWinswitchPayloadTiltedType === 'bool') {config.set.inmsgWinswitchPayloadTilted = config.set.inmsgWinswitchPayloadTilted === 'true'}
+
+			if (config.set.inmsgWinswitchPayloadClosedType === 'num') {config.set.inmsgWinswitchPayloadClosed = Number(config.set.inmsgWinswitchPayloadClosed)}
+			else if (config.set.inmsgWinswitchPayloadClosedType === 'bool') {config.set.inmsgWinswitchPayloadClosed = config.set.inmsgWinswitchPayloadClosed === 'true'}
+
+
+
+
 
 		this.on('input', function(msg,send,done) {
 
